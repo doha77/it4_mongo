bcd9bec65166b55b516b60e866aef208ebd08d0a
Antoine07 <antoine.lucsko@gmail.com>
2021-12-23 12:49:43 +0100

    exercices

--- Chap1_introduction/Exercices/list_exercices.js
+++ Chap1_introduction/Exercices/list_exercices.js
@@ -11,3 +11,25 @@ db.restaurants.find({ cuisine : "Italian", "grades.score" :  10 }).count()
  */
 
 db.restaurants.find({ cuisine : "Italian", "grades.score" :  10 }, {"grades.score" : 1, "address.coord" : 1, name: 1, _id: 0}).sort({name : 1}).pretty()
+
+
+// 03 Quels sont les restaurants qui ont eu un grade A et un score supérieur ou égal à 20 ? Affichez uniquement les noms et ordonnez les par ordre décroissant. Affichez le nombre de résultat.
+
+db.restaurants.find(
+    {
+        "grades.grade": "A",
+        "grades.score": { $gte: 20 }
+    },
+    {
+        _id: 0,
+        "grades.grade": 1,
+        "grades.score": 1
+    }).sort({ name: -1 });
+
+
+//  04. A l'aide de la méthode distinct trouvez tous les quartiers distincts de NY.
+db.restaurants.distinct('borough');
+
+// 05 Trouvez tous les types de restaurants dans le quartiers du Bronx. Vous pouvez là encore utiliser distinct et un deuxième paramètre pour préciser sur quel ensemble vous voulez appliquer cette close.
+
+db.restaurants.distinct('cuisine', { "borough": "Bronx" });
