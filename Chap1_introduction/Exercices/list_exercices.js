f017a47eabfb1fb7f108f399ae3a7250b8c522f6
Antoine07 <antoine.lucsko@gmail.com>
2021-12-24 12:39:46 +0100

    regex

--- Chap1_introduction/Exercices/list_exercices.js
+++ Chap1_introduction/Exercices/list_exercices.js
@@ -223,3 +223,22 @@ db.restaurants.find({
         print(`name : ${name.toUpperCase()} coordinate : ${address.coord.join(' ')}`)
     }
 )
+
+
+/**
+ * 
+1. Affichez la liste des restaurants dont le nom commence et se termine par une voyelle.
+
+2. Affichez la liste des restaurants dont le nom commence et se termine par une même lettre. Vous ferez attention à ne pas récupérer dans votre requête les restaurants n'ayant pas de nom. 
+ */
+
+// /^[aeiou].*[aeiou]$/
+
+
+// 1
+db.restaurants.find({ name : /^[aeiou].*[aeiou]$/i }, { name: 1, _id : 0});
+
+
+// 2
+// \w <=> [a-zA-Z_0-9]
+db.restaurants.find({ name : /^(\w).*\1$/i }, { name: 1, _id : 0});
