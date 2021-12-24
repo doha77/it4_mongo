2987ccaa3affea1bf2ae459a0a6175e844461cc3
Antoine07 <antoine.lucsko@gmail.com>
2021-12-24 11:54:20 +0100

    gps

--- Chap1_introduction/Exercices/list_exercices.js
+++ Chap1_introduction/Exercices/list_exercices.js
@@ -208,4 +208,18 @@ db.restaurants.createIndex({"address.coord" : "2dsphere"}) ; // calcul géométr
 const METERS_PER_MILE = 1609.34; // 1 mile en metre
 const coordinate = [-73.961704, 40.662942]; // dans le planc car on est en 2DSphere
 
-db.restaurants.find({ $nearSphere: { $geometry: { type: "Point", coordinates: coordinate }, $maxDistance: 5*METERS_PER_MILE } }).pretty();
+db.restaurants.find({ 
+    "address.coord": 
+    { $nearSphere: 
+        { $geometry: { type: "Point", coordinates: coordinate }, 
+        $maxDistance: 5 * METERS_PER_MILE }
+    } 
+}, {borough :1, name : 1, "address.coord" : 1 , _id : 0}).forEach(
+    doc => {
+        const { name, address, borough} = doc;
+        print("----------------------------------")
+        print(`Borough : ${borough.toUpperCase()}`)
+        print()
+        print(`name : ${name.toUpperCase()} coordinate : ${address.coord.join(' ')}`)
+    }
+)
