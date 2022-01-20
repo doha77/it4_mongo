33e37ad5eb8bda241591f7e7f9a5ed52e9dc497a
Antoine07 <antoine.lucsko@gmail.com>
2022-01-20 10:10:23 +0100

    aggregate

--- Chap1_introduction/Exercices/restaurants_aggregate.js
+++ Chap1_introduction/Exercices/restaurants_aggregate.js
@@ -2,11 +2,14 @@
  On cherche le nombre de restaurants japonais par quartier
 **/
 db.restaurants.aggregate([
-    {$match : { cuisine : "Japanese"}},
-    {$group : {
-        _id : "$borough", total : { $sum : 1 }
-    }}
+    { $match: { cuisine: "Japanese" } },
+    {
+        $group: {
+            _id: "$borough", total: { $sum: 1 }
+        }
+    }
 ]);
+
 
 /**
 { "_id" : "Brooklyn", "total" : 153 }
@@ -16,3 +19,46 @@ db.restaurants.aggregate([
 { "_id" : "Bronx", "total" : 17 }
 { "_id" : "Queens", "total" : 117 }
  */
+
+db.restaurants.aggregate([
+    // restriction 
+    // $match champ et une/des conditions avec des opérateurs Mongo
+    { $match: { cuisine: { $in: ["Japanese", "Italian"] } } },
+    // grouper par rapport à un champ ici les quartiers attention ce champ est une variable pour Mongo, puis on applique une fonction d'aggrégation sur le groupement. Ici on compte +1 dès que l'on trouve des restaurants Japanese ou Italian
+    {
+        $group: {
+            _id: "$borough", total: { $sum: 1 }
+        }
+    }
+]);
+
+// propre pour la console
+db.restaurants.aggregate([
+    { $match: { cuisine: { $in: ["Japanese", "Italian"] } } },
+    {
+        $group: {
+            _id: "$borough", total: { $sum: 1 }
+        }
+    }
+]);
+
+// pour vérifier que l'on a le bon nombre recherché de restaurants :
+db.restaurants.aggregate([
+    { $match: { cuisine: "Italian" } },
+    {
+        $group: {
+            _id: "$borough", total: { $sum: 1 }
+        }
+    }
+]);
+
+db.restaurants.aggregate([
+    { $match: { cuisine: "Japanese" } },
+    {
+        $group: {
+            _id: "$borough", total: { $sum: 1 }
+        }
+    }
+]);
+
+// Exercice donnez le nombre de restaurant(s) par type de cuisine à NY
